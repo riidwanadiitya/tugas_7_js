43be3e1ad77138681f37d88bd3ab9120d91f5bf8
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 08:55:47 +0700

    TUGAS 7 JS

---
+++ tugas7.js
@@ -0,0 +1,12 @@
+var tinggiBadan = [166, 167, 168, 169, 170, 171, 172, 173, 174, 175]
+
+
+for (i = 0; i < tinggiBadan.length; i++) {
+    console.log(tinggiBadan[i])
+}
+
+console.log("")
+
+for (let x of tinggiBadan) {
+    console.log(x)
+}
